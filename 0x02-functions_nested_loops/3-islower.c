0bd4992b76e018fbeb07b8d6b751c0490c3b103a
cjay16 <chelseadebeer25@gmail.com>
2022-03-25 14:02:06 -0700

    execute

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+
+/**
+ *_islower - checks for lowercase
+ *@c:
+ */
+
+
+
+int _islower(int c)
+
+{
+
+	if (c >= 'a' && c <= 'z')
+
+	{
+
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+}
