99b409754b8e171de146cf6d4d78a731857f9b03
cjay16 <chelseadebeer25@gmail.com>
2022-03-28 14:44:11 -0700

    exe

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+#define NULL 0
+
+
+
+/**
+ *_strpbrk - searches a string for any of a set of bytes
+ */
+
+
+
+char *_strpbrk(char *s, char *accept)
+
+{
+
+	int i, j;
+
+
+
+	for (i = 0; *(s + i) != '\0'; i++)
+
+	{
+
+		for (j = 0; *(accept + j) != '\0'; j++)
+
+		{
+
+			if (*(s + i) == *(accept + j))
+
+			{
+
+				return (s + i);
+
+			}
+
+		}
+
+	}
+
+
+
+	return (NULL);
+
+}
