06df47a3110513adad58eb0d5737670bea129824
cjay16 <chelseadebeer25@gmail.com>
2022-04-14 13:46:20 -0700

    print numbers

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,37 @@
+#ifndef VARIADIC_FUNCTIONS_H
+
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+
+
+/**
+ *struct op - S
+ */
+
+
+
+typedef struct op
+
+{
+
+	char c;
+
+	void (*f)(va_list);
+
+} op_t;
+
+
+
+void print_all(const char * const format, ...);
+
+#endif
